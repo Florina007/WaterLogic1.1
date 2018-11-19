fc75213896a0d20b044c146ee146011eb2e4849c
Alexandru-Constantin Petrovics <1067647@ucn.dk>
2018-11-19 14:05:41 +0100

    Add project files.

---
+++ WaterLogicc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WaterLogicc.Startup))]
+namespace WaterLogicc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
